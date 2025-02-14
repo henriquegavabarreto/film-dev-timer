acc847b04a9ce217a259da0ddff1e3793d7d0096
Henrique Gava <henriquegavabarreto@gmail.com>
2025-02-14 10:57:04 -0800

    adjust home and about styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,28 +3,28 @@ export default function About() {
         <div>
             <h1 className="m-5 text-2xl font-bold text-center">About This Project</h1>
             <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">Why Film Development?</h3>
-            <p className="m-10 w-1/2">
+            <p className="mx-10 w-1/2">
                 I'm passionate about the creative process in general. Whether it's software, art, baked goods, 
                 or anything else, I love the magic of transformation—turning ideas into reality. 
                 Film development has a special kind of charm compared to the digital form: the process of 
                 capturing moments and revealing them in silver halide is enchanting. Once you experience it, you're hooked.
             </p>
             <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">Another Darkroom Clock?</h3>
-            <p className="m-10 w-1/2">
+            <p className="mx-10 w-1/2">
                 While there are several darkroom clock apps available, none quite fit my needs. 
                 I've been using my phone's stopwatch for timing and a notepad for recipes, constantly 
                 switching between tools. This inspired me to create a unified solution that works for 
                 me—and hopefully for others, too.
             </p>
             <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">Who Am I?</h3>
-            <p className="m-10 w-1/2">
+            <p className="mx-10 w-1/2">
                 I'm a creative and curious individual who uses software development to solve practical 
                 problems, craft useful tools, and build fun and enjoyable experiences. You can find some 
                 of my other projects in
                  <a className="text-blue-600" href="https://github.com/henriquegavabarreto" target="_blank"> my github</a>.
             </p>
             <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">How Was This Made?</h3>
-            <p className="m-10 w-1/2">
+            <p className="mx-10 w-1/2">
                 This web app was built using TypeScript, Next.js, Tailwind CSS, and Firebase.
             </p>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
     <div>
       <h1 className="m-5 text-2xl font-bold text-center">My Film Development Workflow</h1>
       <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">What Is This?</h3>
-      <p className="mx-10 my-5 w-1/2">
+      <p className="mx-10 w-1/2">
           This web application is a simple tool designed to organize your film development workflows 
           and information. It generates timers for each step of the process and helps you keep track 
           of your workflow usage over time.
       </p>
       <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">What Are Workflows?</h3>
-      <p className="mx-10 my-5 w-1/2">
+      <p className="mx-10 w-1/2">
           Workflows are sequences of timed steps that guide your film development process. Since every 
           photographer's approach is unique, workflows are fully customizable.<br/>
           After creating an account and logging in, you can create new workflows and access them in your 
@@ -19,12 +19,12 @@ export default function Home() {
           chemical used, your agitation method, and any other relevant information to support your process.
       </p>
       <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">What is History For?</h3>
-      <p className="mx-10 my-5 w-1/2">
+      <p className="mx-10 w-1/2">
           It is a way to track your workflow use. After completing a development process, you can save it 
           to your history and add a note for future reference, if desired.
       </p>
       <h3 className="mx-10 mb-5 mt-10 text-xl font-semibold">My screen locks even when the timer is on. How do I fix this?</h3>
-      <p className="mx-10 my-5 w-1/2">
+      <p className="mx-10 w-1/2">
           Change the display auto-lock settings of your device.
       </p>
     </div>
